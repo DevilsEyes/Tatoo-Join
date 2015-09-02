187ee6c5ef1709466ef8005e886187f20aa4aa8f
DevilsEyes <DevilsEyes@git.com>
2015-09-02 10:39:22 +0800

    测试通过(目前依然是连接测试服)
    1.  优化交互，现在点在input附近也会获得input的焦点
    
    2.  流程修改，因为ios不保存session，所以将验证登陆与加入合并为一次请求。验证码页面总是推迟到最后。
    
    3.样式的小修改

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var DATA = {
     oldCN: '',
     companyName: '',
-    companyAvatar:'',
+    companyAvatar: '',
     nickname: '',
     n: 0,
     avatar: '',
@@ -13,8 +13,8 @@ var DATA = {
 };
 
 //var BaseUrl = 'http://api.meizhanggui.cc/V1.0.0/';
+var BaseUrl = 'http://123.57.42.13/V2.0.0/';
 var _id = location.search.substr(1).match(/_id=([^\b&]*)/)[1];//获取机构id...之类的
-var BaseUrl = 'http://123.57.42.13/V1.0.0/';
 
 var putTemp = function (ele, data) {
     var html = $('#' + ele).html(),
@@ -22,6 +22,8 @@ var putTemp = function (ele, data) {
         i = 0,
         param = '',
         ReplaceArray = [];
+    if(args==null)return;
+    html = html.replace(/<loadimg/g,'<img');
     for (i = 0; i < args.length; i++) {
         param = args[i].substr(2, args[i].length - 4);
         if (data[param]) {
@@ -37,24 +39,24 @@ var putTemp = function (ele, data) {
     $('#' + ele).html(html);
 };
 
-var myJsonp = function (obj,pageObj) {
+var myJsonp = function (obj, pageObj) {
     $.jsonp({
         url: obj.url,
-        callbackParameter: obj.callbackParameter?obj.callbackParameter:"callback",
-        data:obj.data?obj.data:null,
+        callbackParameter: obj.callbackParameter ? obj.callbackParameter : "callback",
+        data: obj.data ? obj.data : null,
         success: obj.success,
         error: obj.error ? obj.error : function () {
             layer.msg('您的网络连接不太顺畅哦!');
         },
-        beforeSend:pageObj?function(){
+        beforeSend: pageObj ? function () {
             pageObj.isClick = true;
-                $('#loading').show();
+            $('#loading').show();
 
-        }:null,
-        complete: pageObj?function () {
+        } : null,
+        complete: pageObj ? function () {
             $('#loading').hide();
             pageObj.isClick = false;
-        }:null
+        } : null
     })
 };
 
@@ -77,13 +79,16 @@ var nowPage = function (str) {
             $('#choose .btn').click(page_choose.btn$click);
             break;
     }
+    $('.inputRow').click(function () {
+        $(this).find('input').focus();
+    });
     $('#' + str).show();
 };
 
 var result = function (status, msg) {
 
     DATA.resultImg = status;
-    DATA.resultStr = status=='success'?('您已成功加入' + DATA.companyName + '！'):msg;
+    DATA.resultStr = status == 'success' ? ('您已成功加入' + DATA.companyName + '！') : msg;
     DATA.resultColor = status == 'fail' ? '#ff0000' : '#84c579';
 
     nowPage('finish');
@@ -97,36 +102,39 @@ window.addEventListener('load', function () {
     //获取基本信息
     myJsonp({
         url: BaseUrl + "Company/info?_method=GET",
-        data:{
-            conpanyId:_id,
-            clerks:true
+        data: {
+            companyId: _id,
+            clerks: true
         },
         success: function (obj) {
             obj = $.parseJSON(obj);
 
             if (obj.code == 0) {
+                console.dir(obj);
 
-                if(obj.data.companyInfo.switch==0){
-                    return result('fail','该店铺暂时不接受纹身师加入!');
+                if (obj.data.company.switch == 0) {
+                    return result('fail', '该店铺暂时不接受纹身师加入!');
                 }
-                DATA.companyAvatar = obj.data.companyInfo.logo?obj.data.companyInfo.logo + '?imageView2/0/w/160':'imgs/def_avatar.jpg';
-                DATA.companyName = obj.data.companyInfo.name;
-                DATA.n = obj.data.companyInfo?obj.data.companyInfo.clerkIdList:0;
+                DATA.companyAvatar = obj.data.company.logo ? obj.data.company.logo + '?imageView2/0/w/160' : 'imgs/def_avatar.jpg';
+                DATA.companyName = obj.data.company.name;
+                DATA.n = obj.data.company ? obj.data.company.clerkIdList.length : 0;
                 DATA.joinList = obj.data.userList;
 
                 var $other = $('#entry .other');
                 if (DATA.joinList.length > 0) {
                     for (var i = 0; i < DATA.joinList.length; i++) {
-                        if(DATA.joinList[i].avatar == ''){
+                        if (DATA.joinList[i].userId == obj.data.company.ownerId)DATA.nickname = DATA.joinList[i].nickname;
+
+                        if (DATA.joinList[i].avatar == '') {
                             DATA.joinList[i].avatar = 'imgs/def_avatar.jpg';
                         }
-                        else{
+                        else {
                             DATA.joinList[i].avatar += '?imageView2/0/w/160';
                         }
                         $other.append(
                             '<div class="row">'
-                            + '<div class="subavatar"><img src="'+DATA.joinList[i].avatar+'"/></div>'
-                            + '<div class="f32 c1a">'+DATA.joinList[i].nickname+'</div>'
+                            + '<div class="subavatar"><img src="' + DATA.joinList[i].avatar + '"/></div>'
+                            + '<div class="f32 c1a">' + DATA.joinList[i].nickname + '</div>'
                             + '</div>'
                         );
                     }
@@ -151,7 +159,7 @@ var page_entry = {
     isClick: false,
     btn$click: function () {
         if (this.isClick)return;
-        var ph = $('#entry #phonenum').attr('value');
+        var ph = $('#entry #phonenum').val();
         if (ph.length != 11) {
             layer.msg('请输入正确的手机号');
             return;
@@ -168,16 +176,35 @@ var page_entry = {
 
                     if (obj.code == 0) {
 
-                        if(obj.data.userInfo){//已注册，验证身份
-                            if(obj.data.userInfo.avatar == ''){
+                        if (obj.data.userInfo) {//已注册，验证身份
+                            if (obj.data.userInfo.avatar == '') {
                                 DATA.avatar = 'imgs/def_avatar.jpg';
-                            }else{
+                            } else {
                                 DATA.avatar = obj.data.userInfo.avatar + '?imageView2/0/w/160';
                             }
                             DATA.nickname = obj.data.userInfo.nickname;
-                            nowPage('join');
+
+                            if (parseInt(obj.data.userInfo.sector) != 30) {
+                                //不是纹身师
+                                result('fail', '只有纹身师可以加入店铺！');
+                            }
+                            else {
+                                if (obj.data.companyId) {
+                                    //有所属的纹身师
+                                    if (obj.data.companyId == _id) {
+                                        result('fail', '您已经在该机构中。');
+                                    }
+                                    else {
+                                        nowPage('choose');
+                                    }
+                                }
+                                else {
+                                    //无所属的纹身师
+                                    nowPage('join');
+                                }
+                            }
                         }
-                        else{//尚未注册
+                        else {//尚未注册
                             nowPage('register');
                         }
 
@@ -186,7 +213,7 @@ var page_entry = {
                     }
 
                 }
-            },page_entry);
+            }, page_entry);
         }
     }
 };
@@ -196,40 +223,42 @@ var page_register = {
     vecTime: 0,
     btn$click: function () {
         if (this.isClick)return;
-        var nickname = $('#register .nickname').attr('value');
-        var captcha = $('#register .vec').attr('value');
+        var nickname = $('#register .nickname').val();
+        var captcha = $('#register .vec').val();
         if (captcha.length < 6) {
             layer.msg('请输入完整的验证码！');
             return;
         }
 
         myJsonp({
-            url: BaseUrl + "User/h5Register?_method=PUT",
+            url: BaseUrl + "Company/Clerk?_method=PUT",
             data: {
+                companyId: _id,
                 phonenum: DATA.phonenum,
                 nickname: nickname,
-                captcha: captcha
+                captcha: captcha,
+                register: true
             },
             success: function (obj) {
                 obj = $.parseJSON(obj);
 
                 if (obj.code == 0) {
-
                     layer.msg('注册成功!');
+
                     result('success');
 
                 } else {
                     layer.msg(obj.msg);
                 }
             }
-        },page_register)
+        }, page_register)
 
     },
     vec$click: function () {
-        if (this.isClick || this.vecTime > 0)return;
+        if (this.isClick || page_register.vecTime > 0)return;
 
         myJsonp({
-            url: BaseUrl + "User/h5RegisterCaptcha?_method=GET",
+            url: BaseUrl + "User/registerCaptcha?_method=GET",
             data: {
                 phonenum: DATA.phonenum
             },
@@ -243,6 +272,7 @@ var page_register = {
                     $('#register .getVec').addClass('disable');
                     window.timer = setInterval(function () {
                         page_register.vecTime--;
+                        $('#register .getVec').text('剩余' + page_register.vecTime + '秒');
                         if (page_register.vecTime == 0) {
                             clearInterval(timer);
                             $('#register .getVec').removeClass('disable');
@@ -254,80 +284,47 @@ var page_register = {
                     layer.msg(obj.msg);
                 }
             }
-        },page_register);
+        }, page_register);
     }
 };
 
 var page_join = {
-    isClick:false,
+    isClick: false,
     vecTime: 0,
     btn$click: function () {
         if (this.isClick)return;
-        var captcha = $('#join .vec').attr('value');
+        var captcha = $('#join .vec').val();
         if (captcha.length < 6) {
             layer.msg('请输入完整的验证码！');
             return;
         }
 
         myJsonp({
-            url: BaseUrl + "User/login?_method=POST",
+            url: BaseUrl + "Company/Clerk?_method=PUT",
             data: {
+                companyId:_id,
                 phonenum: DATA.phonenum,
-                captcha: captcha
+                captcha: captcha,
+                login: true
             },
             success: function (obj) {
                 obj = $.parseJSON(obj);
-
                 if (obj.code == 0) {
-
-                    if(parseInt(obj.data.ownUserInfo.sector)!=30){
-                        //不是纹身师
-                        result('fail', '只有纹身师可以加入店铺！');
-                    }
-                    else{
-                        if(obj.data.ownUserInfo.companyId){
-                            //有所属的纹身师
-                            if(obj.data.ownUserInfo.companyId==_id){
-                                result('fail','您已经在该机构中。');
-                            }
-                            else{
-                                nowPage('choose');
-                            }
-                        }
-                        else{
-                            //无所属的纹身师
-                            myJsonp({
-                                url:BaseUrl + "Company/clerk?_method=PUT",
-                                data:{
-                                    companyId:_id
-                                },
-                                success:function(obj){
-                                    obj = $.parseJSON(obj);
-
-                                    if (obj.code == 0) {
-                                        result('success');
-                                    } else {
-                                        layer.msg(obj.msg);
-                                    }
-                                }
-                            });
-                            result('success');
-                        }
-                    }
-
-
+                    result('success');
                 } else {
                     layer.msg(obj.msg);
+                    //result('fail',obj.msg);
                 }
+
             }
-        },page_join)
+        }, page_join)
 
     },
     vec$click: function () {
-        if (this.isClick || this.vecTime > 0)return;
+        if (this.isClick || page_join.vecTime > 0)return;
 
         myJsonp({
-            url: BaseUrl + "User/h5RegisterCaptcha?_method=GET",
+            url: BaseUrl + "User/h5LoginCaptcha?_method=GET",
             data: {
                 phonenum: DATA.phonenum
             },
@@ -341,6 +338,7 @@ var page_join = {
                     $('#join .getVec').addClass('disable');
                     window.timer = setInterval(function () {
                         page_join.vecTime--;
+                        $('#join .getVec').text('剩余' + page_join.vecTime + '秒');
                         if (page_join.vecTime == 0) {
                             clearInterval(timer);
                             $('#join .getVec').removeClass('disable');
@@ -352,27 +350,13 @@ var page_join = {
                     layer.msg(obj.msg);
                 }
             }
-        },page_join);
+        }, page_join);
     }
 };
 
 var page_choose = {
-    isClick:false,
-    btn$click:function(){
-        myJsonp({
-            url:BaseUrl + "Company/clerk?_method=PUT",
-            data:{
-                companyId:_id
-            },
-            success:function(obj){
-                obj = $.parseJSON(obj);
-
-                if (obj.code == 0) {
-                    result('success');
-                } else {
-                    layer.msg(obj.msg);
-                }
-            }
-        })
+    isClick: false,
+    btn$click: function () {
+        nowPage('join');
     }
 };
