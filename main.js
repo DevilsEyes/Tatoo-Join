16bd216522d6a3c0050a03a0bbe2ebc7c10132ed
DevilsEyes <DevilsEyes@git.com>
2015-09-02 16:28:24 +0800

    上线

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ var DATA = {
     resultColor: ''
 };
 
-//var BaseUrl = 'http://api.meizhanggui.cc/V1.0.0/';
-var BaseUrl = 'http://123.57.42.13/V2.0.0/';
+var BaseUrl = 'http://api.meizhanggui.cc/V2.0.0/';
+//var BaseUrl = 'http://123.57.42.13/V2.0.0/';
 var _id = location.search.substr(1).match(/_id=([^\b&]*)/)[1];//获取机构id...之类的
 
 var putTemp = function (ele, data) {
